356bdb1e217b19cbc8883c56489af97ba58b8761
root <starlex92@gmail.com>
2023-01-17 15:38:18 +0100

    commit database connection

--- server/database.py
+++ server/database.py
@@ -4,5 +4,5 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from server.models.product_review import ProductReview
 
 async def init_db():
-     client = AsyncIOMotorClient('mongodb://fastapidb:gtchzYV1S9qdTF5fczkFGey4ZN0J0taeymK9UDUrBxjjHYbVvxxARci8Zp5j66gEyPakMd5vuJO8ACDbINHjbw==@fastapidb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@fastapidb@')
+     client = AsyncIOMotorClient('mongodb://fastapidb:DCirO7Bgc33COC1LP3eUee2DPKyecqfwn1glKkDvNdejtFmEYAG0txTvuxa2dRPmgMwnZRUWzccgACDbg20ZJA==@fastapidb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@fastapidb@')
      await init_beanie(database=client.cosmosmongodb, document_models=[ProductReview])
